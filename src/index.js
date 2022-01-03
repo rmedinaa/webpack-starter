a1e4035e55164b0bb9c52c9d55160a271836f781
Ricardo <ricardoma9517@gmail.com>
2022-01-02 19:07:32 -0600

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-
-
-import {saludar} from './js/componentes.js';
 import './styles.css';
+import {Todo,TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
 
-const nombre = 'Ricardo';
 
-saludar('Ricardo');
+export const todoList = new TodoList();
+
+console.log(todoList.todos);
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+//Opción alterna a esta sintaxis
+//todoList.todos.forEach(crearTodoHtml()); Se usa cuando solo hay un argumento
+
+console.log('todos',todoList.todos);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,19 +1,106 @@
-import '../css/componentes.css';
-//import webpacklogo from '../assets/img/webpack-logo.png';
+import { Todo } from "../classes";
+import { todoList } from "../index";
 
-export const saludar = (nombre = 'Sin nombre') =>{
-    console.log('Creando etiqueta h1');
+//Referencias html
+const divTodoList = document.querySelector('.todo-list');
+const txtInput = document.querySelector('.new-todo');
+const btnBorrar = document.querySelector('.clear-completed');
+const ulFiltros = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro')
 
-    const h1 = document.createElement('h1');
+export const crearTodoHtml = ( todo ) => {
 
-    h1.innerText= `Hola, ${nombre}`;
+    const htmlTodo = `<li class="${(todo.completado)? "completed": ""}" data-id="${todo.id}">
+    <div class="view">
+        <input class="toggle" type="checkbox" ${(todo.completado)? "checked": ""}>
+        <label>${todo.tarea}</label>
+        <button class="destroy"></button>
+    </div>
+    <input class="edit" value="Create a TodoMVC template">
+</li>`
 
-    document.body.append(h1);
+const div = document.createElement('div');
+div.innerHTML = htmlTodo;
 
-    //Img
-    // console.log(webpacklogo);
-    // const img = document.createElement('img');
-    // img.src = webpacklogo;
-    // document.body.append( img );
+divTodoList.append( div.firstElementChild );
+
+return div.firstElementChild;
 
 }
+
+//Eventos
+
+//Agregar un todo
+txtInput.addEventListener('keyup',(event)=>{
+    if (event.keyCode === 13 && txtInput.value.length > 0){
+        console.log(txtInput.value);
+        const nuevoTodo = new Todo(txtInput.value);
+        todoList.nuevoTodo(nuevoTodo);
+        crearTodoHtml(nuevoTodo);
+        txtInput.value = "";
+    }
+});
+
+//Marcar completados y eliminar tareas
+divTodoList.addEventListener('click', (event) =>{
+    const nombreElemento = event.target.localName; //input, label, boton
+    const todoElemento = event.target.parentElement;
+    const todoId = todoElemento.parentElement.getAttribute('data-id');
+    console.log(todoId);
+    
+    if( nombreElemento.includes('input') ){//click en el check
+
+        todoList.marcarCompletado(todoId);
+        todoElemento.parentElement.classList.toggle('completed');
+
+    } else if (nombreElemento.includes('button') ){
+
+        todoList.eliminarTodo(todoId);
+        divTodoList.removeChild(todoElemento.parentElement);
+    }
+});
+
+//Eliminar todos los completados
+btnBorrar.addEventListener('click',()=>{
+    todoList.eliminarCompletados();  
+
+    for( let i = divTodoList.children.length-1; i>=0; i-- ){
+
+        const elemento = divTodoList.children[i];
+        console.log(elemento);
+        if(elemento.classList.contains('completed')){
+            divTodoList.removeChild(elemento);
+        }
+    }
+});
+
+ulFiltros.addEventListener('click',(event)=>{
+
+    const  filtro = event.target.text;
+
+    if( !filtro ){return;};
+
+    anchorFiltros.forEach(elem => elem.classList.remove('selected'));
+    event.target.classList.add('selected');
+    
+    for(const elemento of divTodoList.children){
+
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch(filtro){
+            case 'Pendientes':
+                if(completado) {
+                    elemento.classList.add('hidden');
+                };
+            break;
+            case 'Completados':
+                if(!completado) {
+                    elemento.classList.add('hidden');
+                };
+            break;
+
+        }
+    }
+})
+
